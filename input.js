beecf45ca9cdb1ecfa4c1bebe962ca9772382980
MichaelDHuy <dachuy87@email.com>
2022-09-29 00:49:14 +0000

    Input module added

---
+++ input.js
@@ -0,0 +1,31 @@
+const client = require("./client")
+let connection = client.connect();
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+const handleUserInput = function (key) {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();   
+  };
+  if (key === "w") {     
+    connection.write("Move: up");   
+  };
+  if (key === "s") {     
+    connection.write("Move: down");   
+  };
+  if (key === "a") {     
+    connection.write("Move: left");   
+  };
+  if (key === "d") {     
+    connection.write("Move: right");   
+  };
+console.log(key);
+};
+
+module.exports = {setupInput}; 
